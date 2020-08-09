e9b0358ccd7af5125bc0d255b25121ce5b64046c
dragon-web <1582943265@qq.com>
2020-08-09 11:04:36 +0800

    day_8_8

--- day_8_8/day_8_8/day_8_8.cpp
+++ day_8_8/day_8_8/day_8_8.cpp
@@ -101,7 +101,7 @@ int main()
 //在返回对象时使用复制构造函数
 
 
-
+/*
 class my_string
 {
 	char *s;
@@ -126,12 +126,14 @@ my_string::my_string(const char* str)
 	cout << "Allocating room for s\n";
 	strcpy(s, str);
 }
+
 my_string::my_string(const my_string &obj)
 {
 	s = new char[strlen(obj.s) + 1];
 	strcpy(s, obj.s);
 	cout << "Copy constructor called. \n";
 }
+
 
 void display(my_string ob)
 {
@@ -155,8 +157,75 @@ int main()
 	system("pause");
 	return 0;
 }
-//解决方法是返回一个对象或指针的引用
+//解决方法是返回一个对象或指针的引用 深拷贝
+*/
 
+
+
+
+
+class my_string
+{
+	char *s;
+public:
+	my_string(const char *str);
+	//my_string(const my_string& obj);
+	void operator= (const my_string& obj);
+
+	~my_string()
+	{
+		if (s)
+			delete[] s;
+		cout << "Free s\n" << endl;
+	}
+	void show()
+	{
+		cout << "show:" << endl;
+		cout << s << endl;
+	}
+};
+my_string::my_string(const char* str)
+{
+	s = new char[strlen(str) + 1];
+	cout << "Allocating room for s\n";
+	strcpy(s, str);
+}
+
+/*
+my_string::my_string(const my_string& obj)
+{
+	s = new char[strlen(obj.s) +1];
+	strcpy(s, obj.s);
+	cout << "复制构造" << endl;
+}*/
+
+my_string input()
+{
+	char str[80];
+	cout << "Enter String:" << endl;
+	cin >> str;
+
+	my_string ob(str);
+	return ob;
+}
+void display(my_string& ob)
+{
+	ob.show();
+}
+void my_string::operator= (const my_string& obj)
+{
+	s = new char[strlen(obj.s) + 1];
+	strcpy(s, obj.s);
+	cout << "重载等号" << endl;
+}
+
+int main()
+{
+	my_string obj = input();
+	obj.show();
+	system("pause");
+	return 0;
+}
 
 
 
