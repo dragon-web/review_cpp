b2dc438804f95d982706da2c627579e828b7aa66
dragon-web <1582943265@qq.com>
2020-08-23 17:38:26 +0800

    Project

---
+++ Project4/Project4/test.cpp
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<stdio.h>
+#include<string.h>
+#include<Windows.h>
+/*
+int fun(int p[5][5])
+{
+	int i = 0, j = 0;
+	int min = p[0][0];
+	int num;
+	for (i = 0, j = 0; i < 5 && j < 5; ++i, ++j)
+	{
+		if (p[i][j] < min)
+		{
+			num = p[i][j];
+			p[i][j] = min;
+			min = p[i][j];
+		}
+	}
+	return min;
+}
+void reverse(char s[])
+{
+	int n = strlen(s);
+	int i;
+	char t;
+	for (i = 0; i <= n / 2; i++)
+	{
+		t = *(s + i);
+		*(s + i) = *(s + n - i - 1);
+		*(s + n - i - 1) = t;
+	}
+}*/
+/*
+int peaches(int m, int n)
+{
+	int res = 1;
+	int pre = 1;
+	for (int day = n; day > 0; day--) {
+		res = (3 + pre) * m;   // next 表示后一天
+		pre = res;
+	}
+	return res;
+}
+
+*/
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int a[3][3] = { {3,5},{8,9},{12,35} };
+	int *q = a[0];
+	cout << q[4] << endl;
+	system("pause");
+	return 0;
+}
+
+
