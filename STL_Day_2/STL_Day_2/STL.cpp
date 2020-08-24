32cd0b8b894ff73fd4ad03e7910eb31d97081898
dragon-web <1582943265@qq.com>
2020-08-24 08:07:34 +0800

    STL_Day_2

---
+++ STL_Day_2/STL_Day_2/STL.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<iostream>
+
+using namespace std;
+struct st_t {
+	int status;
+	short *pdata;
+	char errstr[32];
+};
+
+int main()
+{
+	st_t st[16];
+	char *p = (char *)(st[2].errstr + 32);
+	printf("%d", (p - (char *)(st)));
+	system("pause");
+	return 0;
+}
