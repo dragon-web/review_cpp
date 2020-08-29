99a559d67baf927d8d832b413976946504cbaa28
dragon-web <1582943265@qq.com>
2020-08-29 10:02:42 +0800

    Graph

---
+++ Graph/Graph/Graph.cpp
@@ -0,0 +1,17 @@
+#include"common.h"
+
+//void (*signal(int, void(*)(int)))(int);
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+int main()
+{
+
+	int a;
+	char c;
+	float f;
+	scanf("%d,%c,%f", &a, &c, &f);
+
+
+	system("pause");
+	return 0;
+}
---
+++ Graph/Graph/common.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<string>
+#include<stack>
+#include<queue>
+#include<stdio.h>
+#include<signal.h>
+using namespace std;
+
+#define _CRT_SECURE_NO_WARNINGS 
+
+
