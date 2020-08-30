c39e14f5862a23321c7abb1a1d18c0815fd1c0fc
dragon-web <1582943265@qq.com>
2020-08-30 12:59:21 +0800

    Graph

--- Graph/Graph/Graph.cpp
+++ Graph/Graph/Graph.cpp
@@ -1,15 +1,12 @@
 #include"common.h"
 
-//void (*signal(int, void(*)(int)))(int);
-#define _CRT_SECURE_NO_WARNINGS 
+
+
 #include<stdio.h>
 int main()
 {
 
-	int a;
-	char c;
-	float f;
-	scanf("%d,%c,%f", &a, &c, &f);
+	
 
 
 	system("pause");
