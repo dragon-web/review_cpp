fe95f494c1780ce1a9737ae11d0da5a8803186ad
dragon-web <1582943265@qq.com>
2020-09-01 22:06:19 +0800

    装配路径算法

---
+++ 装配路径算法/装配路径算法/Path.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+//AOEÍøÓëÍØÆËÅÅĞò
+
+#include"path.h"
+
+
+
+int main()
+{
+
+	
+	system("pause");
+	return 0;
+}
---
+++ 装配路径算法/装配路径算法/path.h
@@ -0,0 +1,80 @@
+#pragma once
+#define DataType char
+#define MAX_VERTEX 10
+#include<iostream>
+#include<stack>
+#include<string>
+
+using namespace std;
+
+struct ArcNode  //边表
+{
+	int adjvex;     //记录下一个顶点标号
+	ArcNode *next;
+	unsigned int  IN;//记录入度
+};
+
+struct VertexNode  //顶点表
+{
+	DataType vertex;
+	ArcNode *firstEdge;
+};
+class ALGraph
+{
+private:
+	VertexNode adjust[MAX_VERTEX];
+	int vertexNum, arcNum;  //顶点个数  边的个数
+public:
+	ALGraph(DataType v[], int n, int e);
+	void printTable(VertexNode *p, int n, int e);
+	VertexNode* GetVerTexNode()
+	{
+		return this->adjust;
+	}
+};
+
+ALGraph::ALGraph(DataType v[], int n, int e)
+{
+	vertexNum = n;
+	arcNum = e;
+	for (int i = 0; i < vertexNum; ++i)
+	{
+		adjust[i].vertex = v[i];
+		adjust[i].firstEdge = NULL;
+	}
+	for (int j = 0; j < arcNum; j++)
+	{
+		int vi;
+		int vj;
+		unsigned int WEIGHT;
+		cout << "请输入两个相连顶点编号以及两点的路径:(中间以空格隔开)" << endl;
+		cin >> vi >> vj >> WEIGHT;
+		ArcNode * s;
+		s = new ArcNode;
+		s->adjvex = vj;  //存储终点
+		s->next = adjust[vi].firstEdge;
+		s->weight = WEIGHT;
+		adjust[vi].firstEdge = s;
+	}
+}
+
+void ALGraph::printTable(VertexNode *p, int n, int e)
+{
+	VertexNode* temp = p;
+	for (int i = 0; i < n; ++i)
+	{
+		auto temp1 = temp->firstEdge;
+		cout << temp->vertex << "->";
+		while (temp1 != NULL)
+		{
+			cout << arr[temp1->adjvex] << "->";
+			temp1 = temp1->next;
+		}
+		cout << "NULL" << endl;
+		temp++;
+	}
+}
+
+
+
+
