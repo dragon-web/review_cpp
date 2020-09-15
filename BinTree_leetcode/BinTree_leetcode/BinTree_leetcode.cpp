685ed7ee7f9e5b9f8e088dd6065fffea85c05bb1
dragon-web <1582943265@qq.com>
2020-09-15 22:06:44 +0800

    BinTree_leetcode

---
+++ BinTree_leetcode/BinTree_leetcode/BinTree_leetcode.cpp
@@ -0,0 +1,115 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+/*
+  struct TreeNode {
+	  int val;
+	  TreeNode *left;
+	  TreeNode *right;
+	  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+class Solution {
+public:
+	int Size(TreeNode* root)
+	{
+		if (root == NULL)
+			return 0;
+		else
+		{
+			return Size(root->left) + Size(root->right) + 1;
+		}
+	}
+	vector<int> preorderTraversal(TreeNode* root) {
+		int N = Size(root);
+		vector<int> res(N);
+		stack<TreeNode*> S;
+		S.push(root);
+		int i = 0;
+		while (S.size())
+		{
+			TreeNode* p = S.top();
+			S.pop();
+			if (p->right != NULL)
+				S.push(p->right);
+			if (p->left != NULL)
+				S.push(p->left);
+			res[i] = p->val;
+			i++;
+		}
+		return res;
+	}
+};
+*/
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ 
+
+
+  class Solution {
+  public:
+	  int Size(TreeNode *root)
+	  {
+		  if (root == NULL)
+			  return 0;
+		  return Size(root->left) + Size(root->right) + 1;
+	  }
+	  vector<int> inorderTraversal(TreeNode* root) {
+		  int N = Size(root);
+		  vector<int> res(N);
+		  if (N == 0)
+		  {
+			  return res;
+		  }
+		  else
+		  {
+			  int i = 0;
+			  TreeNode* q = root;
+			  stack<TreeNode*> dp;
+			  do {
+				  if (dp.size())
+					  q = dp.top();
+				  while (q != NULL)
+				  {
+					  dp.push(q);
+					  q = q->left;
+				  }
+				  if(dp.size())
+				    q = dp.top();
+					  res[i] = q->val;
+					  i++;
+				  dp.pop();
+				  if (q->right != NULL)
+				  {
+					  dp.push(q->right);
+				  }
+			  } while (dp.size()|| q != NULL);
+		  }
+		  return res;
+	  }
+  };
+
+
+  int main()
+  {
+	  Solution a;
+	  TreeNode a1(1);
+	  TreeNode a2(2);
+	  TreeNode a3(3);
+	  TreeNode a4(4);
+	  a1.left = &a2;
+	  a2.left = &a4;
+	  a1.right = &a3;
+	  vector<int> res = a.inorderTraversal(&a1);
+	  system("pause");
+	  return 0;
+  }
