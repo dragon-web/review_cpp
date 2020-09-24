6999503ab529b06218e6e2a4487d43a70fbfe616
dragon-web <1582943265@qq.com>
2020-09-24 21:29:49 +0800

    newleetcode

--- leetcode_day_9_18/leetcode_day_9_18/delete.cpp
+++ leetcode_day_9_18/leetcode_day_9_18/delete.cpp
@@ -97,7 +97,7 @@ int main()
 	return 0;
 }
 
-*/
+
 
 
 using namespace std;
@@ -175,6 +175,56 @@ int main()
 		cout << res->val << " ";
 		res = res->next;
 	}
+	system("pause");
+	return 0;
+}
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int Count_One(int num)
+	{
+		int tes = 0;
+		while (num != 0)
+		{
+			if (num % 10 == 1)
+			{
+				tes++;
+
+			}
+			num /= 10;
+
+		}
+		return tes;
+
+	}
+
+	int NumberOf1Between1AndN_Solution(int n)
+	{
+		int sum = 0;
+		int count = 0;
+		for (int i = 1; i <= n; ++i)
+		{
+			count = Count_One(i);
+			sum += count;
+			count = 0;
+
+		}
+		return sum;
+	}
+};
+
+int main()
+{
+	int num = 13;
+	Solution a;
+	int res = a.NumberOf1Between1AndN_Solution(num);
+	cout << res << endl;
 	system("pause");
 	return 0;
 }
