3bad336b2cb1b061821a07bfb4800fab70d5bc06
dragon-web <1582943265@qq.com>
2020-10-16 18:10:09 +0800

    template

--- Advanced_template/Advanced_template/Advanced_template.cpp
+++ Advanced_template/Advanced_template/Advanced_template.cpp
@@ -2,10 +2,27 @@
 
 #include<iostream>
 
+
+using namespace std;
+
+template<class T>
+class Test
+{
+private:
+	T m_data;
+public:
+	void Print(T data);
+};
+
+template<class T>
+void Test<T>::Print(T data)
+{
+	cout << "Test" << endl;
+}
 int main()
 {
-
-
+	Test<int> t;
+	t.Print(0);
 	system("pause");
 	return 0;
 }
