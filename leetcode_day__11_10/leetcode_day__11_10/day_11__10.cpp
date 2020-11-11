264066b426a5fffb4882ceab23e306bbb806d2b8
dragon-web <1582943265@qq.com>
2020-11-11 19:45:25 +0800

    leetcode_day_11_11

--- leetcode_day__11_10/leetcode_day__11_10/day_11__10.cpp
+++ leetcode_day__11_10/leetcode_day__11_10/day_11__10.cpp
@@ -40,7 +40,7 @@ public:
 		return (dx == dy) && (px != py);
 	}
 };
-
+*/
 
 
 
@@ -71,10 +71,10 @@ public:
 	}
 };
 
-*/
+
 
 using namespace std;
-
+/*
 class Solution {
 public:
 	int add(vector<int> nums)
@@ -119,4 +119,29 @@ int main()
 
 	system("pause");
 	return 0;
-}
+}*/
+
+
+
+class Solution {
+public:
+	void dfs(TreeNode* root, TreeNode* p, int d, int x, int& depth, TreeNode** parent) {
+		if (root == NULL) return;
+		if (root->val == x) {
+			*parent = p;
+			depth = d;
+			return;
+		}
+		dfs(root->left, root, d + 1, x, depth, parent);
+		dfs(root->right, root, d + 1, x, depth, parent);
+	}
+	bool isCousins(TreeNode* root, int x, int y) {
+		int dx = 0;
+		int dy = 0;
+		TreeNode* px = NULL;
+		TreeNode* py = NULL;
+		dfs(root, NULL, 0, x, dx, &px);
+		dfs(root, NULL, 0, y, dy, &py);
+		return (dx == dy) && (px != py);
+	}
+};
